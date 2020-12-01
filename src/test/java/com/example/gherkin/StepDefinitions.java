3587506cbe35e6d8710d933b8e28d592abab94b5
cmlozanos <carlosmlozanos@gmail.com>
2020-12-01 17:21:51 +0100

    fix test for get request

--- src/test/java/com/example/gherkin/StepDefinitions.java
+++ src/test/java/com/example/gherkin/StepDefinitions.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class StepDefinitions {
@@ -13,8 +14,9 @@ public class StepDefinitions {
 
 	@When("users want to get information on the {string} project")
 	public void users_want_to_get_information_on_the_project(final String string) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		this.server.startAndConfigureMockServer();
+		this.response = RestAssured.get(this.server.getHost() + "/users").thenReturn();
+		this.server.stop();
 	}
 
 	@Then("the requested data is returned")

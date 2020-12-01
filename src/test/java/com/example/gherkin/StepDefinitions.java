f9e8c8b5143cf2fc84d52fb67191eaece881c268
cmlozanos <carlosmlozanos@gmail.com>
2020-12-01 17:41:35 +0100

    add test for user response with a single user

--- src/test/java/com/example/gherkin/StepDefinitions.java
+++ src/test/java/com/example/gherkin/StepDefinitions.java
@@ -1,10 +1,13 @@
 package com.example.gherkin;
 
+import org.eclipse.jetty.http.HttpStatus;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Assertions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class StepDefinitions {
@@ -22,6 +25,9 @@ public class StepDefinitions {
 	@Then("the requested data is returned")
 	public void the_requested_data_is_returned() {
 		Assertions.assertNotNull(this.response);
+		this.response.then().statusCode(HttpStatus.OK_200);
+		this.response.then().contentType(ContentType.JSON);
+		this.response.then().body("name", CoreMatchers.equalTo("user1"));
 	}
 
 }

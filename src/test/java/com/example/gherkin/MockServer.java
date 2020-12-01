1c91574dc3e799cf7209bcc8863c1793ddde2a59
cmlozanos <carlosmlozanos@gmail.com>
2020-12-01 17:45:22 +0100

    fix test for multiple users with names, user1 and user2

--- src/test/java/com/example/gherkin/MockServer.java
+++ src/test/java/com/example/gherkin/MockServer.java
@@ -16,8 +16,8 @@ public class MockServer {
 	public void startAndConfigureMockServer() {
 		this.wireMockServer.start();
 		WireMock.configureFor("localhost", this.wireMockServer.port());
-		WireMock.stubFor(
-				WireMock.get(WireMock.urlEqualTo("/users")).willReturn(WireMock.okJson("{\"name\":\"user1\"}")));
+		WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/users"))
+				.willReturn(WireMock.okJson("[{\"name\":\"user1\"}, {\"name\":\"user2\"}]")));
 		this.host = "http://localhost:" + this.wireMockServer.port();
 	}
 

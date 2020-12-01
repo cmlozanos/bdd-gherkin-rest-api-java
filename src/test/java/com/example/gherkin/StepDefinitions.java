f60d5f18344986a5b1ccbc9150902236629f4da1
cmlozanos <carlosmlozanos@gmail.com>
2020-12-01 16:35:40 +0100

    Copy Junit error to Step Definitios

--- src/test/java/com/example/gherkin/StepDefinitions.java
+++ src/test/java/com/example/gherkin/StepDefinitions.java
@@ -1,11 +1,19 @@
 package com.example.gherkin;
 
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import static org.junit.Assert.*;
-
 public class StepDefinitions {
 
+	@When("users want to get information on the {string} project")
+	public void users_want_to_get_information_on_the_project(final String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the requested data is returned")
+	public void the_requested_data_is_returned() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
 }

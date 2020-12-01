bd05b5e717ad1222e13db4b4c6878c08da55d6fb
cmlozanos <carlosmlozanos@gmail.com>
2020-12-01 17:14:06 +0100

    add Assert for response

--- src/test/java/com/example/gherkin/StepDefinitions.java
+++ src/test/java/com/example/gherkin/StepDefinitions.java
@@ -1,9 +1,15 @@
 package com.example.gherkin;
 
+import org.junit.jupiter.api.Assertions;
+
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
 
 public class StepDefinitions {
+
+	private Response response;
+	private final MockServer server = new MockServer();
 
 	@When("users want to get information on the {string} project")
 	public void users_want_to_get_information_on_the_project(final String string) {
@@ -13,7 +19,7 @@ public class StepDefinitions {
 
 	@Then("the requested data is returned")
 	public void the_requested_data_is_returned() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		Assertions.assertNotNull(this.response);
 	}
+
 }

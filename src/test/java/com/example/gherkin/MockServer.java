aaf60bab019e26004b2bfe05cab1926aa46e6aa1
cmlozanos <carlosmlozanos@gmail.com>
2020-12-02 07:40:10 +0100

    add stubFor User1 and refactorize USERS

--- src/test/java/com/example/gherkin/MockServer.java
+++ src/test/java/com/example/gherkin/MockServer.java
@@ -6,6 +6,10 @@ import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 
 public class MockServer {
 
+	private static final String USER_1 = "{\"name\":\"user1\"}";
+	private static final String USER_2 = "{\"name\":\"user2\"}";
+	private static final String ALL_USERS = "[" + MockServer.USER_1 + ", " + MockServer.USER_2 + "]";
+
 	private final WireMockServer wireMockServer;
 	private String host;
 
@@ -16,8 +20,8 @@ public class MockServer {
 	public void startAndConfigureMockServer() {
 		this.wireMockServer.start();
 		WireMock.configureFor("localhost", this.wireMockServer.port());
-		WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/users"))
-				.willReturn(WireMock.okJson("[{\"name\":\"user1\"}, {\"name\":\"user2\"}]")));
+		WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/users")).willReturn(WireMock.okJson(MockServer.ALL_USERS)));
+		WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/users/1")).willReturn(WireMock.okJson(MockServer.USER_1)));
 		this.host = "http://localhost:" + this.wireMockServer.port();
 	}
 

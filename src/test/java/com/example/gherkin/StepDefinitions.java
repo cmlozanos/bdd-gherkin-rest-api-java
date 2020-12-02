36b3f3a7fcde877624053b824c65938dde4deeef
cmlozanos <carlosmlozanos@gmail.com>
2020-12-02 07:29:59 +0100

    change StepDefinitions

--- src/test/java/com/example/gherkin/StepDefinitions.java
+++ src/test/java/com/example/gherkin/StepDefinitions.java
@@ -1,9 +1,10 @@
 package com.example.gherkin;
 
-import org.eclipse.jetty.http.HttpStatus;
+import org.apache.http.HttpStatus;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Assertions;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
@@ -15,20 +16,27 @@ public class StepDefinitions {
 	private Response response;
 	private final MockServer server = new MockServer();
 
-	@When("users want to get information on the {string} project")
-	public void users_want_to_get_information_on_the_project(final String string) {
+	@Given("server is started")
+	public void server_is_started() {
 		this.server.startAndConfigureMockServer();
-		this.response = RestAssured.get(this.server.getHost() + "/users").thenReturn();
-		this.server.stop();
 	}
 
-	@Then("the requested data is returned")
-	public void the_requested_data_is_returned() {
+	@When("perform GET operation to retrieve users from {string}")
+	public void perform_get_operation_to_retrieve_users_from(final String path) {
+		this.response = RestAssured.get(this.server.getHost() + path).thenReturn();
+	}
+
+	@Then("all users will be returned")
+	public void all_users_will_be_returned() {
 		Assertions.assertNotNull(this.response);
-		this.response.then().statusCode(HttpStatus.OK_200);
+		this.response.then().statusCode(HttpStatus.SC_OK);
 		this.response.then().contentType(ContentType.JSON);
 		this.response.then().body("[0].name", CoreMatchers.equalTo("user1"));
 		this.response.then().body("[1].name", CoreMatchers.equalTo("user2"));
 	}
 
+	@Then("server will be stoped")
+	public void server_will_be_stoped() {
+		this.server.stop();
+	}
 }

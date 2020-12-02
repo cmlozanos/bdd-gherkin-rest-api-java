1ac95b884edcacb4ca00fd9a9fe47e174edddb1d
cmlozanos <carlosmlozanos@gmail.com>
2020-12-02 13:01:52 +0100

    add user stories add step definitions

--- src/test/java/com/example/gherkin/StepDefinitions.java
+++ src/test/java/com/example/gherkin/StepDefinitions.java
@@ -1,8 +1,16 @@
 package com.example.gherkin;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.apache.http.HttpStatus;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Assertions;
+
+import com.example.gherkin.server.MockServer;
+import com.example.gherkin.server.User;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -13,34 +21,41 @@ import io.restassured.response.Response;
 
 public class StepDefinitions {
 
+	private static final String USERS_PATH = "/users";
+
 	private Response response;
 	private final MockServer server = new MockServer();
-	private String path;
 	private Integer id;
-	private String name;
+	private HashMap<Integer, User> users;
+	private User user;
 
 	@Given("server is started")
 	public void server_is_started() {
-		this.server.startAndConfigureMockServer();
+		this.server.startAndConfigureMockServer(this.users);
 	}
 
-	@Given("path is {string}")
-	public void path_is(final String path) {
-		this.path = path;
+	@Given("users is")
+	public void users_is(final Map<Integer, String> users) {
+		this.users = new HashMap<Integer, User>();
+		users.forEach((id, name) -> this.users.put(id, new User(id, name)));
 	}
 
 	@When("retrieve all users")
 	public void retrieve_all_users() {
-		this.response = RestAssured.get(this.server.getHost() + this.path).thenReturn();
+		this.response = RestAssured.get(this.server.getHost() + StepDefinitions.USERS_PATH).thenReturn();
 	}
 
 	@Then("all users will be returned")
 	public void all_users_will_be_returned() {
 		Assertions.assertNotNull(this.response);
+		this.response.then().log().all();
 		this.response.then().statusCode(HttpStatus.SC_OK);
 		this.response.then().contentType(ContentType.JSON);
-		this.response.then().body("[0].name", CoreMatchers.equalTo("user1"));
-		this.response.then().body("[1].name", CoreMatchers.equalTo("user2"));
+		final List<User> values = this.users.values().stream().collect(Collectors.toList());
+		for (int i = 0; i < this.users.size(); i++) {
+			this.response.then().body("[" + i + "].id", CoreMatchers.equalTo(values.get(i).getId()));
+			this.response.then().body("[" + i + "].name", CoreMatchers.equalTo(values.get(i).getName()));
+		}
 	}
 
 	@Given("user id is {int}")
@@ -50,13 +65,15 @@ public class StepDefinitions {
 
 	@When("retrieve user by id")
 	public void retrieve_user_by_id() {
-		this.response = RestAssured.get(this.server.getHost() + this.path + "/" + this.id).thenReturn();
+		this.response = RestAssured.get(this.server.getHost() + StepDefinitions.USERS_PATH + "/" + this.id)
+				.thenReturn();
 	}
 
 	@Then("user will be found")
 	public void user_will_be_found() {
 		Assertions.assertNotNull(this.response);
 		this.response.then().contentType(ContentType.JSON);
+		this.response.then().body("id", CoreMatchers.equalTo(this.id));
 		this.response.then().body("name", CoreMatchers.equalTo("user" + this.id));
 	}
 
@@ -88,20 +105,21 @@ public class StepDefinitions {
 		this.response.then().statusCode(statusCode);
 	}
 
-	@Given("User with name {string}")
-	public void user_with_name(final String name) {
-		this.name = name;
+	@Given("User with id {int} name {string}")
+	public void user_with_id_name(final Integer id, final String name) {
+		this.user = new User(id, name);
 	}
 
 	@When("add new user")
 	public void add_new_user() {
-		final String user = String.format("{\"name\" : \"%s\"}", this.name);
-		this.response = RestAssured.given().body(user).post(this.server.getHost() + this.path).thenReturn();
+		this.response = RestAssured.given().body(this.user).post(this.server.getHost() + StepDefinitions.USERS_PATH)
+				.thenReturn();
 	}
 
 	@Then("user id will be returned")
 	public void user_id_will_be_returned() {
 		Assertions.assertNotNull(this.response);
-		this.response.then().header("location", CoreMatchers.equalTo(this.server.getHost() + this.path + "/" + 10));
+		this.response.then().log().all().header("location",
+				CoreMatchers.equalTo(this.server.getHost() + StepDefinitions.USERS_PATH + "/" + 10));
 	}
 }
